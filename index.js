dda3bb052aebc66d3b128146f678474236c27c3e
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-12 10:47:35 +0300

    flatten_js_browserified

---
+++ index.js
@@ -0,0 +1,15 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import {Point, Vector, Circle, Line, Ray, Segment, Arc, Box, Polygon, Matrix, PlanarSet} from '@flatten-js/core';
+
+window.Point = Point;
+window.Vector = Vector;
+window.Circle = Circle;
+window.Line = Line;
+window.Ray = Ray;
+window.Segment = Segment;
+window.Arc = Arc;
+window.Box = Box;
+window.Polygon = Polygon;
+window.Matrix = Matrix;
+window.PlanarSet = PlanarSet;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path'); // Подключаем встроенный модуль 'path' для работы с путями файлов
+const webpack = require('webpack'); // Подключаем 'webpack' для использования его функционала
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: './index.js', // Указываем точку входа для приложения
+    output: {
+        filename: 'flatten_js_browserified.js', // Имя выходного файла после сборки
+        path: path.resolve(__dirname, 'dist'), // Путь к директории, куда будет помещен собранный файл
+        library: 'flatten_js_browserified', // Имя экспортируемой библиотеки
+        libraryTarget: 'var', // Тип экспорта библиотеки (в данном случае как переменная)
+        globalObject: 'this' // Для поддержки браузеров и Node.js
+    },
+    mode: 'development', // Устанавливаем режим сборки в 'development' для удобства отладки
+    resolve: {
+        fallback: {
+            "assert": false, // Отключаем использование модуля 'assert'
+            "process": require.resolve("process/browser") // Указываем замену для модуля 'process' в браузере
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser', // Автоматически подключаем модуль 'process' для использования в коде
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'disabled', // Отключаем автоматическое открытие браузера
+            generateStatsFile: true, // Генерируем файл stats.json
+            statsFilename: 'stats.json'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Применяем правило ко всем .js файлам
+                exclude: /node_modules/, // Исключаем папку node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [//автоматическое добавление необходимых полифиллов
+                            ['@babel/preset-env', {
+                                useBuiltIns: 'entry', // Используем полифиллы на основе импортов
+                                corejs: 3 // Указываем версию core-js
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+
+};
